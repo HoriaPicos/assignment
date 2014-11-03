a3f934a2c66330cd877d78a5d249f51a1a0e7307
Adi Cheo <adi.cheo@gmail.com>
2014-11-02 20:11:48 -0500

    2 bacons, one chasing mouse, the other chasing the formet.

--- a71.py
+++ a71.py
@@ -1,10 +1,12 @@
 import pygame
 import sys
 import os
+import math
 from pygame.locals import *
  
 pygame.mixer.init()
 white = (255,255,255)
+
  
 '''Used to load resources'''
 
@@ -34,21 +36,51 @@ def load_sounds(name):
     return sound
     
 class Bacon(pygame.sprite.Sprite):
-    def __init__(self):
-        pygame.sprite.Sprite.__init__(self)
-        self.image, self.rect = load_pic('bacon.png')
+    def __init__(self, speed, toggle):
         
+        pygame.sprite.Sprite.__init__(self)
+        
+        self.speed = speed
+        self.toggle = toggle
+
+        if not self.toggle:
+            self.image, self.rect = load_pic('bacon.png')
+        else:
+            self.image, self.rect = load_pic('bacon.png')
+               
+        self.image = pygame.transform.scale(self.image, (50, 50))
+        self.image = pygame.transform.rotate(self.image, 90)
+        self.velocity = [0, 0];
+        self.speed = speed
+        self.toggle = toggle
+
     def update(self):
-        pos = pygame.mouse.get_pos()
-        self.rect.midtop = pos
+        if not self.toggle:
+            target = pygame.mouse.get_pos()
+        else:
+            target = self.toggle.topleft
+        #print "mous: "
+        #print target
+        
+        v = target[0] - self.rect.topleft[0], target[1] - self.rect.topleft[1]
+        norm = math.sqrt(v[0]**2 + v[1]**2) * self.speed
+
+        if not ((int(v[0]) >= -5 and int(v[0]) <= 5 and int(v[1]) >= -5 and int(v[1]) <= 5)):
+            unit_vec = 10 * v[0] / norm, 10 * v[1] / norm
+            self.velocity = (((self.velocity[0] *.9) + unit_vec[0]), ((self.velocity[1] *.9) + unit_vec[1]))
+
+            self.rect.topleft = self.rect.topleft[0] + self.velocity[0], self.rect.topleft[1]+ self.velocity[1]
+        
+        #print "rect: "
+        #print self.rect.topleft
     
-    
+        
 #Bacon MOvement
 def main():
     pygame.init()
     screen = pygame.display.set_mode((1200,800))
     pygame.display.set_caption('Bacon Runner')
-    pygame.mouse.set_visible(0)
+    pygame.mouse.set_visible(1)
     
     background = pygame.Surface(screen.get_size())
     background = background.convert()
@@ -57,8 +89,9 @@ def main():
     screen.blit(background, (0, 0))
     pygame.display.flip()
     clock = pygame.time.Clock()
-    bacon = Bacon()
-    allsprites = pygame.sprite.RenderPlain((bacon))
+    bacon = Bacon(5, False)
+    bacon2 = Bacon(10, bacon.rect)
+    allsprites = pygame.sprite.Group(bacon, bacon2)
     
     #MAIN LOOP
     while 1:
